66e79682a76e518890e390bb417e232074fb8fd3
zhuog123 <zhuog287@gmail.com>
2024-11-23 21:23:48 +0800

    調整

--- js/index.js
+++ js/index.js
@@ -1,31 +1,42 @@
 //登入&註冊的密碼查看
-
-
-
-
-let test = document.querySelectorAll('.password')
-test.forEach(function (item,index ) {
-    item.addEventListener('click', (e) => {
-        if (e.target.matches('span')) {
-            // 獲取兩個 span
-            const eyeCloseSpan = item.querySelector('.humbleicons--eye-close');
-            const eyeOpenSpan = item.querySelector('.mage--eye');
-            const input = item.querySelector('input')
-            // 根據點擊的 span 進行相應操作
-            if (e.target.classList.contains('humbleicons--eye-close')) {
-                // 處理關閉眼睛的 span
-                eyeCloseSpan.style.display = 'none';
-                eyeOpenSpan.style.display = 'block';
-                input.type = 'text';
-                // 其他操作...
-            } else if (e.target.classList.contains('mage--eye')) {
-                // 處理打開眼睛的 span
-                eyeCloseSpan.style.display = 'block';
-                eyeOpenSpan.style.display = 'none';
-                input.type = 'password';
-                // 其他操作...
-
-            }
-        }
-    })
+$('.password').on('click', 'span', function () {
+    let password = $(this).parent();
+    let eyeClose = password.find('.humbleicons--eye-close')
+    let eyeOpen = password.find('.mage--eye');
+    let input = password.find('input');
+    if ($(this).hasClass('humbleicons--eye-close')) {
+        eyeClose.toggleClass('show');
+        eyeOpen.toggleClass('show');
+        input.attr('type', 'text');
+    } else if ($(this).hasClass('mage--eye')) {
+        eyeClose.toggleClass('show');
+        eyeOpen.toggleClass('show');
+        input.attr('type', 'password');
+    }
 })
+
+//回到頂部
+
+// let test = document.querySelector('.toTop')
+// test.addEventListener('click', function () {
+//     window.scrollTo({
+//         top: 0,
+//         left: 0,
+//         behavior: "smooth"
+//     })
+// })
+
+$('.toTop').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 500);
+})
+$(window).scroll(function () {
+    //獲得一個可視窗口的高
+    let height = $(this).height();
+    // 如果垂直滾動超過高，.toTop 就出現
+    if ($(this).scrollTop() >= height / 2) {
+        $('.toTop').show();
+    } else {
+        $('.toTop').hide();
+    }
+})
+
